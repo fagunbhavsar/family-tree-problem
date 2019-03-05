141e91dce09580d5a91a9f7e2ccfa8e8a9a7b361
Fagun Bhavsar <fagun.bhavsar@gmail.com>
2019-03-05 15:05:31 +0530

    Added few more specs - factorial

--- PlayGround/Rspec/lib/bowling.rb
+++ PlayGround/Rspec/lib/bowling.rb
@@ -1,8 +1,12 @@
 class Bowling
 	attr_reader :score
 
+	def initialize
+		@score = 0
+	end
+
 	def hit(pin_count)
-		
+		@score += pin_count	
 	end
 
 end 
---
+++ PlayGround/Rspec/lib/factorial.rb
@@ -0,0 +1,5 @@
+class Factorial
+  def factorial_of
+
+  end
+end
