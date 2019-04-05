bf1c26e89d7177fe95ec8cd4e09df289372eb2e9
Fagun Bhavsar <fagun.bhavsar@gmail.com>
2019-04-05 15:24:56 +0530

    Modified factorial spec

--- PlayGround/Rspec/lib/factorial.rb
+++ PlayGround/Rspec/lib/factorial.rb
@@ -1,2 +1,16 @@
+class Factorial
 
+  def initialize(n)
+    @n = n
+  end
+
+  def calculate
+    result = 1
+    while @n > 1 do
+      result = result * @n
+      @n = @n - 1
+    end
+    result
+  end
+end
 
--- PlayGround/Rspec/spec/factorial_spec.rb
+++ PlayGround/Rspec/spec/factorial_spec.rb
@@ -1,18 +1,4 @@
-class Factorial
 
-  def initialize(n)
-    @n = n
-  end
-
-  def calculate
-      result = 1
-      while @n > 1 do
-        result = result * @n
-        @n = @n - 1
-      end
-      result
-    end
-  end
 
 describe Factorial do
 
