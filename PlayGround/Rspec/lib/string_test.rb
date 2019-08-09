49a224197d59bb7995d9bc4fe98a180124d6d34e
Fagun Bhavsar <github.fagun@gmail.com>
2019-08-09 11:13:06 +0530

    Added more spec examples

--- PlayGround/Rspec/lib/string_test.rb
+++ PlayGround/Rspec/lib/string_test.rb
@@ -1,1 +1,10 @@
+def upper_case(str)
+  if str =~ /^[A-Z]/
+    puts "String contains uppercase letter"
+  else
+    puts "String contains only lowercase letters"
+  end
+end
 
+upper_case("Abcd")
+upper_case("abcd")
